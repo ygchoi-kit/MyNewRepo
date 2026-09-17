04c9c3c8916b33cac53a9b6c567b3acc1f5d2b0a
ygchoi-kit <ygchoi1111@kumoh.ac.kr>
2026-09-17 10:38:45 +0900

    Two files have been modified

--- BinarySearch.py
+++ BinarySearch.py
@@ -3,17 +3,17 @@ x = input('Input a number: ')
 left = 0
 right = len(a) - 1
 
-# Find x in the list a
+
 while left <= right:
     middle = (left + right) // 2
   
-    if a[middle] == int(x):  # Success
-        print('There is {:3} at a[{:2}] in the list.'.format(x, middle))
+    if a[middle] == int(x):
+        print('1.There is {:3} at a[{:2}] in the list.'.format(x, middle))
         break   
     elif a[middle] < int(x):
         left = middle + 1    
     else:
         right = middle - 1
 
-if left > right: # Fail
-    print('There is not {:3} in this list.'.format(x))
+if left > right:
+    print('2.There is not {:3} in this list.'.format(x))
--- QuickSort.py
+++ QuickSort.py
@@ -4,7 +4,7 @@ def qsort(a, low, high):
         qsort(a, low, pivot-1)
         qsort(a, pivot+1, high)
 
-
+# Partition algorithm for the quick sort
 def partition(a, pivot, high):
     i = pivot+1
     j = high
@@ -22,7 +22,7 @@ def partition(a, pivot, high):
     a[pivot], a[j] = a[j], a[pivot]
     return j
 
-
+# Test codes
 a = [54,88,77,26,93,17,49,10,17,77,11,31,22,44,17,20]
 print('정렬 전:\t', a)  
 qsort(a, 0, len(a)-1)
